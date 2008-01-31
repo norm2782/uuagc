e3251effda173b6aeb3f0a5533e98f264025a6e2
amiddelk <amiddelk@30b93d28-2606-0410-b7f1-95e0edb40354>
2008-01-31 15:41:31 +0000

    Adapted makefiles of uuagc for buildfarm
    git-svn-id: https://svn.science.uu.nl/repos/project.STEC.uuagc/uuagc/trunk@255 30b93d28-2606-0410-b7f1-95e0edb40354

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+# builds uuagc with the uuagc compiler in PATH
+
+echo "** Running autoconf"
+autoconf
+
+echo "** Bootstrapping with existing uuagc compiler"
+./configure
+make build
+make clean
+
+echo "** Using new uuagc compiler to compile ag files"
+./configure --with-ag=`pwd`/dist/build/uuagc/uuagc
+make derived
